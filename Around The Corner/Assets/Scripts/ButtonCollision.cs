41e343b5df4c66c5d7d5d73377325601acd92507
S4C-Olympus <54639306+S4C-Olympus@users.noreply.github.com>
2020-02-24 17:57:22 -0700

    Main cameras changed and things done

---
+++ Around The Corner/Assets/Scripts/ButtonCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCollision : MonoBehaviour
+{
+    public Material activeButton; 
+    private void Start()
+    {
+        GetComponent<Collider>();
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && Input.GetButtonDown("Display 1 Fire"))
+        {
+
+            Debug.Log("Acive");
+        }
+    }
+}
