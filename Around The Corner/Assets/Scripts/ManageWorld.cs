73e771f8b2b04137eccf115ff8d51c6eef960c50
KIERAN BORLAND <u1136227@umail.utah.edu>
2020-02-24 21:40:35 -0700

    Added the timer UI for each player

---
+++ Around The Corner/Assets/Scripts/ManageWorld.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageWorld : MonoBehaviour
+{
+    private float timer;
+
+    public Text display1Timer;
+
+    public Text display2Timer;
+
+    public Text display3Timer;
+
+    public Text display4Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        display1Timer.text = "Time: " + timer;
+        display2Timer.text = "Time: " + timer;
+        display3Timer.text = "Time: " + timer;
+        display4Timer.text = "Time: " + timer;
+    }
+}
