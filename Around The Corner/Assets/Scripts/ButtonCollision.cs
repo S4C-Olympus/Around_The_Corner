ef4130d2b68b0801579783f3d0988a9bd6936ed9
S4C-Olympus <54639306+S4C-Olympus@users.noreply.github.com>
2020-02-25 03:15:24 -0700

    Prototype Puzzle Dne

--- Around The Corner/Assets/Scripts/ButtonCollision.cs
+++ Around The Corner/Assets/Scripts/ButtonCollision.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.SceneManagement;
 
 public class ButtonCollision : MonoBehaviour
 {
     public Movable_Piece move_platform;
+    public int buttonDisplay;
     private void Start()
     {
         //GetComponent<Collider>();
     }
     public void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag == "Player" && Input.GetButtonDown("Display 1 Fire"))
+        if(other.gameObject.tag == "Player" && Input.GetButtonDown("Display " + buttonDisplay+ " Fire"))
         {
-            move_platform.TriggerMove();
-            Debug.Log("Acive");
+            if (Input.GetButtonDown("Display 4 Fire"))
+            {
+                Application.Quit();
+            }
+            else
+            {
+
+                move_platform.TriggerMove();
+                Debug.Log("Acive");
+            }
         }
     }
 }
--- Around The Corner/Assets/Scripts/Crate.cs
+++ Around The Corner/Assets/Scripts/Crate.cs
@@ -4,22 +4,73 @@ using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("a"))
+        if (Input.GetAxis("Display 2 Vertical") == 1)
         {
-            transform.position = transform.position + new Vector3(-2, 0, 0);
+            transform.position = transform.position + new Vector3(0, 0, 0.1f);
         }
-        if (Input.GetKeyDown("d"))
+        if (Input.GetAxis("Display 2 Vertical") == -1)
         {
-            transform.position = transform.position + new Vector3(2, 0, 0);
+            transform.position = transform.position + new Vector3(0, 0, -0.1f);
         }
     }
+    //private void OnColliderEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        this.GetComponent<Rigidbody>().AddForce(Vector3.left * 11110);
+    //    }
+    //}
+    //public float distance = 1f;
+    //public LayerMask boxMask;
+
+    //GameObject box;
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    Physics2D.queriesStartInColliders = false;
+    //    RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+
+    //    if (hit.collider != null && hit.collider.gameObject.tag == "pushable" && Input.GetKeyDown(KeyCode.E))
+    //    {
+
+
+    //        box = hit.collider.gameObject;
+    //        box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+    //        box.GetComponent<FixedJoint2D>().enabled = true;
+    //        //box.GetComponent<boxpull>().beingPushed = true;
+
+    //    }
+    //    else if (Input.GetKeyUp(KeyCode.E))
+    //    {
+    //        box.GetComponent<FixedJoint2D>().enabled = false;
+    //        //box.GetComponent<boxpull>().beingPushed = false;
+    //    }
+
+    //}
+
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.yellow;
+
+    //    Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
+
+
+
+    //}
+
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    if(collision.gameObject.tag == "Player")
+    //    { 
+    //        if(Input.GetAxis("Display 2 Horizontal") == 1)
+    //        {
+    //            transform.position = transform.position + new Vector3(0, 0, -2);
+    //        }
+    //    }
+    //}
 }
